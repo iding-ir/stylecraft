a20580bc13c829d9e4a56516d180e50db92e3bb3
Aydin Ghane <aydin.ghane.kh@gmail.com>
2020-04-10 21:08:00 +0430

    Add modal module

--- src/js/script.js
+++ src/js/script.js
@@ -2,4 +2,28 @@ $(document).ready(() => {
   $(".sc-slide-toggle").on("click", () => {
     $(".sc-slide").toggleClass("sc-is-open");
   });
+
+  $("#trigger-modal-small").on("click", () => {
+    $("#modal-small").addClass("sc-is-visible");
+  });
+
+  $("#trigger-modal-medium").on("click", () => {
+    $("#modal-medium").addClass("sc-is-visible");
+  });
+
+  $("#trigger-modal-large").on("click", () => {
+    $("#modal-large").addClass("sc-is-visible");
+  });
+
+  $("#trigger-modal-stretched").on("click", () => {
+    $("#modal-stretched").addClass("sc-is-visible");
+  });
+
+  $("#trigger-modal-fullscreen").on("click", () => {
+    $("#modal-fullscreen").addClass("sc-is-visible");
+  });
+
+  $(".sc-modal button").on("click", function () {
+    $(this).parents(".sc-modal").removeClass("sc-is-visible");
+  });
 });
