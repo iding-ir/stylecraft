a0567751709827355e8c1d5a32f63f9cc755177b
Aydin Ghane <aydin.ghane.kh@gmail.com>
2020-04-11 23:35:05 +0430

    Add loader module

--- src/stylecraft.js
+++ src/stylecraft.js
@@ -20,4 +20,4 @@ import "./scss/modules/slide.scss";
 import "./scss/modules/modal.scss";
 import "./scss/modules/divider.scss";
 import "./scss/modules/overlay.scss";
-import "./scss/modules/splash.scss";
+import "./scss/modules/splash-loader.scss";
