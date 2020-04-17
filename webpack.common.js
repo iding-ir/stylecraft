fa7467596f9e2a2e0778d4549f0792e70211d50f
Aydin Ghane <aydin.ghane.kh@gmail.com>
2020-04-17 20:39:32 +0430

    Add favicons and meta tags to documentation

--- webpack.common.js
+++ webpack.common.js
@@ -1,5 +1,5 @@
 const path = require("path");
-var HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const webpack = require("webpack");
 const CopyPlugin = require("copy-webpack-plugin");
 
--- webpack.prod.js
+++ webpack.prod.js
@@ -5,8 +5,8 @@ const CleanWebpackPlugin = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
-var HtmlWebpackPlugin = require("html-webpack-plugin");
-// const CopyPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = merge(common, {
   mode: "production",
@@ -30,12 +30,12 @@ module.exports = merge(common, {
   },
   plugins: [
     new MiniCssExtractPlugin({ filename: "[name].css" }),
-    // new CopyPlugin([
-    //   {
-    //     from: path.resolve(__dirname, "src/assets/fonts"),
-    //     to: path.resolve(__dirname, "dist/assets/fonts"),
-    //   },
-    // ]),
+    new CopyPlugin([
+      {
+        from: path.resolve(__dirname, "src/assets/favicons"),
+        to: path.resolve(__dirname, "dist/assets/favicons"),
+      },
+    ]),
     new CleanWebpackPlugin(),
   ],
   module: {
